3899cf459d707df8e4fb083fc61c00c90e3e2aff
HuiLi2 <Hui.Li2@harman.com>
2017-10-26 18:09:00 +0000

    add Developer Guide

--- setEnv.sh
+++ setEnv.sh
@@ -8,15 +8,17 @@ alias sdk='cd $sdkPATH'
 alias SDK='cd $sdkPATH'
 
 if [ -f "$sdkPATH/environment-setup-corei7-64-elina-linux" ]; then
-	echo "SDK EXIT"
-	echo "source environment-setup-corei7-64-elina-linux"
-	source "$sdkPATH/environment-setup-corei7-64-elina-linux"
+    echo "SDK EXIT"
+    echo "source environment-setup-corei7-64-elina-linux"
+    source "$sdkPATH/environment-setup-corei7-64-elina-linux"
+    echo "CXX" = $CXX
+    echo "CC" = $CC
 else
-	echo "SDK do not EXIT"
-	echo "set CXX = g++"
-	echo "set CC = gcc"
-#export CXX="g++"
-#export CC="gcc"
+    echo "SDK do not EXIT"
+    export CXX="g++"
+    export CC="gcc"
+    echo "set CXX = g++"
+    echo "set CC = gcc"
 fi
 
 alias adas='cd $ADAS_ROOT_PATH'
@@ -30,6 +32,9 @@ alias caseclientlib='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux'
 
 alias abase='cd $ADAS_ROOT_PATH/000_Framework/libABase'
 alias abaselib='cd $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux'
+
+alias graphic='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic/src'
+alias graphiclib='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic/lib/linux'
 
 alias project='cd $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/src/Camera/Test'
 
@@ -48,9 +53,10 @@ alias H='history'
 alias opendir='nautilus .'
 alias OPENDIR='nautilus .'
 
-remoteIP="10.80.105.48"
+remoteIP="10.80.105.55"
 
 alias pushfoundation='sudo scp $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux/libAFoundation.so root@$remoteIP://usr/lib/'
 alias pushabase='sudo scp $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux/libABase.so root@$remoteIP://usr/lib/'
 alias pushcaseclient='sudo scp $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux/libACaseSocket.so root@$remoteIP://usr/lib/'
+alias pushgraphic='sudo scp $ADAS_ROOT_PATH/000_Framework/libAGraphic/lib/linux/libAGraphic.so root@$remoteIP://usr/lib/'
 alias pushproject='sudo scp $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/src/Camera/Test/cameraStateTest root@$remoteIP://usr/bin/'
