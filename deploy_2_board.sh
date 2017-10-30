bd2b6395cd4ea1a787744b159eb340ceb4e74ad1
HuiLi2 <Hui.Li2@harman.com>
2017-10-30 12:26:04 -0100

    modify setEnv.sh

--- deploy_2_board.sh
+++ deploy_2_board.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Board_IP="10.80.105.55"
+remoteIP="10.80.105.55"
 
 #sudo scp -p ./004_ToBoard/camera_setup.sh   root@10.80.105.48://
 #sudo scp -p ./004_ToBoard/ar1043_vc_test.sh   root@10.80.105.48://usr/bin
@@ -10,7 +10,13 @@ Board_IP="10.80.105.55"
 #sudo scp ./004_ToBoard/libmediactl.so.0.0.0     root@10.80.105.48://usr/lib/
 #sudo scp ./004_ToBoard/libv4l2subdev.so.0.0.0    root@10.80.105.48://usr/lib/
 
-sudo scp  $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux/libAFoundation.so root@$Board_IP://usr/lib/
-sudo scp  $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux/libABase.so root@$Board_IP://usr/lib/
-sudo scp  $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux/libACaseSocket.so root@$Board_IP://usr/lib/
-sudo scp -p $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/src/Camera/Test/cameraStateTest root@$Board_IP://usr/bin/
+
+sudo scp  $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux/libAFoundation.so root@$remoteIP://usr/lib/
+sudo scp  $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux/libABase.so root@$remoteIP://usr/lib/
+sudo scp  $ADAS_ROOT_PATH/000_Framework/libAGraphic/lib/linux/libAGraphic.so root@$remoteIP://usr/lib/
+sudo scp  $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux/libACaseSocket.so root@$remoteIP://usr/lib/
+sudo scp  $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/build/gen/libadas-dbus.so root@$remoteIP://usr/lib/
+
+
+sudo scp -p  $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/build/src/adas root@$remoteIP://usr/bin/
+sudo scp -p  $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/src/Camera/Test/cameraStateTest root@$remoteIP://usr/bin/
--- setEnv.sh
+++ setEnv.sh
@@ -6,7 +6,6 @@ cpm_adas_adservice_PATH="$ADAS_ROOT_PATH/../cpm_adas_adservice"
 sdkPATH="/opt/elina/2.0.2015143A"
 
 alias sdk='cd $sdkPATH'
-alias SDK='cd $sdkPATH'
 
 if [ -f "$sdkPATH/environment-setup-corei7-64-elina-linux" ]; then
     echo "SDK EXIT"
@@ -23,16 +22,10 @@ else
 fi
 
 alias adas='cd $ADAS_ROOT_PATH'
-alias ADAS='cd $ADAS_ROOT_PATH'
-
 alias greatwall='cd $cpm_adas_adservice_PATH'
-alias GREATWALL='cd $cpm_adas_adservice_PATH'
 
 alias foundation='cd $ADAS_ROOT_PATH/000_Framework/libAFoundation/src'
 alias foundationlib='cd $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux'
-
-alias caseclient='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/src/SocketIPC'
-alias caseclientlib='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux'
 
 alias abase='cd $ADAS_ROOT_PATH/000_Framework/libABase'
 alias abaselib='cd $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux'
@@ -40,27 +33,31 @@ alias abaselib='cd $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux'
 alias graphic='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic/src'
 alias graphiclib='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic/lib/linux'
 
-alias project='cd $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/Test/Camera'
+alias caseclient='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/src/SocketIPC'
+alias caseclientlib='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux'
+
+alias project='cd $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/'
+
+alias cmakeconfig='cmake -DELINA_LITE_DCIF_GENERATOR=/opt/elinaidelitelinux/elina -DCDEPL_PATH=$ADAS_ROOT_PATH/../contracts-gwm/contracts-gwm-v2mh/packages/adas/ADAS.cdl -DCMAKE_SYSROOT=/opt/elina/2.0.2015143A/sysroots/corei7-64-elina-linux  ../'
 
 alias m='make'
-alias M='make'
 
 alias c='printf "\033c"'
-alias C='printf "\033c"'
 
 alias cm='printf "\033c" && make clean && make'
-alias CM='printf "\033c" && make clean && make'
 
 alias h='history'
-alias H='history'
 
 alias opendir='nautilus .'
-alias OPENDIR='nautilus .'
 
 remoteIP="10.80.105.55"
 
 alias pushfoundation='sudo scp $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux/libAFoundation.so root@$remoteIP://usr/lib/'
 alias pushabase='sudo scp $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux/libABase.so root@$remoteIP://usr/lib/'
-alias pushcaseclient='sudo scp $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux/libACaseSocket.so root@$remoteIP://usr/lib/'
 alias pushgraphic='sudo scp $ADAS_ROOT_PATH/000_Framework/libAGraphic/lib/linux/libAGraphic.so root@$remoteIP://usr/lib/'
+alias pushcaseclient='sudo scp $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux/libACaseSocket.so root@$remoteIP://usr/lib/'
+alias pushadasdbus='sudo scp $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/build/gen/libadas-dbus.so root@$remoteIP://usr/lib/'
+
+
+alias pushbinadas='sudo scp $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/build/src/adas root@$remoteIP://usr/bin/'
 alias pushproject='sudo scp $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/src/Camera/Test/cameraStateTest root@$remoteIP://usr/bin/'
