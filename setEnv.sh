cab5a2679e46430bb620edb64fcd3a0a4860b5e8
HuiLi2 <Hui.Li2@harman.com>
2017-11-10 13:44:46 -0100

    modify setEnv.sh

--- setEnv.sh
+++ setEnv.sh
@@ -32,11 +32,19 @@ if [ -f "$sdkPATH/environment-setup-corei7-64-elina-linux" ]; then
     export CMAKE_SYSROOT="/opt/elina/2.0.2015143A/sysroots/corei7-64-elina-linux"
 
     if [ -d "$CMAKE_SYSROOT/usr/include/adas" ]; then
-        echo "adas file exist"
+		echo "adas(include) dir exist"
     else
         sudo mkdir -p $CMAKE_SYSROOT/usr/include/adas
-        echo "adas file create ok"
+		echo "adas(include) dir create ok"
     fi
+
+	if [ -d "$CMAKE_SYSROOT/usr/lib/adas" ]; then
+		echo "adas(lib) dir exist"
+	else
+	    sudo mkdir -p $CMAKE_SYSROOT/usr/lib/adas
+		echo "adas(lib) dir create ok"
+	fi
+
 else
     echo "SDK do not EXIT"
     export CXX="g++"
@@ -47,8 +55,14 @@ fi
 
 
 alias adas='cd $ADAS_ROOT_PATH'
-alias greatwall='cd $cpm_adas_adservice_PATH'
 alias publish='cd $camfw_gwmv2_PATH'
+if [ -d "$cpm_adas_adservice_PATH/build" ]; then
+    alias project='cd $cpm_adas_adservice_PATH/build'
+else
+    sudo mkdir -p $cpm_adas_adservice_PATH/build
+    alias project='cd $cpm_adas_adservice_PATH/build'
+fi
+alias ftest='cd $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/'
 
 alias foundation='cd $ADAS_ROOT_PATH/000_Framework/libAFoundation/src'
 alias foundationlib='cd $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux'
@@ -56,13 +70,11 @@ alias foundationlib='cd $ADAS_ROOT_PATH/000_Framework/libAFoundation/lib/linux'
 alias abase='cd $ADAS_ROOT_PATH/000_Framework/libABase'
 alias abaselib='cd $ADAS_ROOT_PATH/000_Framework/libABase/lib/linux'
 
-alias graphic='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic/src'
+alias graphic='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic'
 alias graphiclib='cd $ADAS_ROOT_PATH/000_Framework/libAGraphic/lib/linux'
 
 alias caseclient='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/src/SocketIPC'
 alias caseclientlib='cd $ADAS_ROOT_PATH/000_Framework/CaseClient/lib/linux'
-
-alias project='cd $ADAS_ROOT_PATH/001_Project/001_GWMV2MH/'
 
 alias cmakeconfig='cmake -DELINA_LITE_DCIF_GENERATOR=/opt/elinaidelitelinux/elina -DCDEPL_PATH=$ADAS_ROOT_PATH/../contracts-gwm/contracts-gwm-v2mh/packages/adas/ADAS.cdl -DCMAKE_SYSROOT=/opt/elina/2.0.2015143A/sysroots/corei7-64-elina-linux  ../'
 
@@ -74,7 +86,7 @@ alias mc='make clean'
 
 alias cm='printf "\033c" && make clean && make'
 
-alias makeall='foundation && mc  &&  abase && mc  &&  caseclient && mc  &&  graphic && mc  && project && mkdir build &&cd build && rm -fr * && cmakeconfig && make target'
+alias makeall='foundation && mc  &&  abase && mc  &&  caseclient && mc  &&  graphic && mc  && project && rm -fr * && cmakeconfig && make target'
 
 alias h='history'
 
